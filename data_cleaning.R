4df2eed9f75b22ccedc6ffa896fb6266eddd1fe7
John Pearson <jmxpearson@gmail.com>
2015-10-20 14:43:13 -0400

    more data cleaning
    - transform multiple binary-coded columns into two new columns, cued and
      outcome
    - added trial number column

--- data_cleaning.R
+++ data_cleaning.R
@@ -2,6 +2,8 @@
 # this file prepares data for modeling
 
 library(R.matlab)  # for reading Matlab data
+library(dplyr)
+library(tidyr)
 
 datadir <- "data/"
 
@@ -15,7 +17,30 @@ countdata <- as.data.frame(readMat(paste(datadir, countfile, sep=""))[[1]])
 # read column names and merge
 count_colnames_list <- (readMat(paste(datadir, colnamefile, sep=""))[[1]])
 count_colnames <- lapply(count_colnames_list, function(x) {x[[1]][1, 1]})
+count_colnames <- gsub(" ", "_", count_colnames)
 colnames(countdata) <- count_colnames
+
+# add trial number column
+countdata <- countdata %>%
+  mutate(trial=row_number())
+
+# convert multiple binary columns to outcome type and trial type
+outcome <- countdata %>%
+  select(trial, matches("[[:upper:]]{2,}", ignore.case=FALSE)) %>%  # select trial type columns and trial
+  gather(label, flag, -trial) %>%  # collapse multiple columns into one
+  filter(flag == 1) %>%  # select only the "column" corresponding to this trial
+  select(trial, label)  %>% # keep trial and label columns only
+  extract(label, c("cued", "outcome"), "(Cued)*([[:upper:]]{2,})")
+
+# further cleaning
+outcome$cued[is.na(outcome$cued)] <- 0
+outcome$cued[outcome$cued == "Cued"] <- 1
+outcome$outcome <- sapply(outcome$outcome, tolower)
+
+# now merge in new columns
+countdata <- countdata %>%
+  select(-matches("[[:upper:]]{2,}", ignore.case=FALSE)) %>%
+  merge(outcome)
 
 # save as R object
 outfile <- "countdata"
