1a9fd27b39d3c0436503759afddfd38807fc7b40
John Pearson <jmxpearson@gmail.com>
2015-10-27 11:15:00 -0400

    added post hoc analysis code to runmodel
    - should probably pull out later

--- runmodel.R
+++ runmodel.R
@@ -36,10 +36,29 @@ stan_dat <- list(N = length(count)[1],
 
 # get ready to run stan
 watched_pars <- c("beta", "mu", "tau", "Sigma", "nu")
- fit <- stan(file = 'models/model6.stan', data = stan_dat,
+fit <- stan(file = 'models/model6.stan', data = stan_dat,
              pars = watched_pars,
              iter = 1000, thin=2, chains = 8)
 
 # save fit object
-fname <- "fitobj_t_cued"
-save(file=fname, list='fit')
+fname <- "fitobj_norm_targacq_cued"
+save(file=fname, list=c('fit', 'X'))
+
+# make a scatter plot of betas
+fit_summary <- summary(fit, pars='beta')[[1]]
+pt_betas <- fit_summary[,6]  # medians
+U <- length(unique(unit))
+P <- dim(X)[2]
+dim(pt_betas) <- c(U, P)
+colnames(pt_betas) <- colnames(X)
+pairs(pt_betas)
+
+# extract correlation matrix
+fit_summary <- summary(fit, pars='Sigma')[[1]]
+pt_Sigma <- fit_summary[,6]  # medians
+P <- dim(X)[2]
+dim(pt_Sigma) <- c(P, P)
+colnames(pt_Sigma) <- colnames(X)
+rownames(pt_Sigma) <- colnames(X)
+
+# plot
