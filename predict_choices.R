f2c8b4e0775e78bd98fb3b941c16261052c20841
John Pearson <jmxpearson@gmail.com>
2015-10-30 17:29:13 -0400

    added code to fit elastic net prediction models
    - using spikes in each unit to predict choice (binarized to
      pro-/anti-social

---
+++ predict_choices.R
@@ -0,0 +1,58 @@
+# train some machine learning models to predict choices from spike counts
+library(glmnet)
+library(dplyr)
+library(caret)
+
+set.seed(12345)
+
+# load data
+load("data/countdata")
+
+# filter data with too few observations
+countdata <- countdata %>% group_by(unit) %>% filter(length(outcome) > 100)
+
+partition_data <- function(outcome, nfolds) {
+  suppressMessages(require('caret'))
+
+  folds <- createFolds(outcome, nfolds)
+
+  foldid <- rep(NA, length(outcome))
+
+  for (ind in 1:nfolds) {
+    foldid[folds[[ind]]] <- ind
+  }
+
+  return(foldid)
+}
+
+# set epoch of interest
+countvar <- "TargetAcquire_sp_count"
+
+# model to train
+form <- as.formula(paste(" ~ ", countvar))
+
+# define function to train glm
+train_glm <- function(df, nfolds) {
+  folds <- partition_data(df$y, nfolds)
+  tryCatch({
+    fit <- cv.glmnet(model.matrix(form, data=df), df$y, family="binomial",
+              type.measure = "auc", foldid = folds)
+    maxlambda <- fit$lambda.1se
+    maxind <- which(fit$lambda == fit$lambda.1se)
+    return(c(fit$cvm[maxind], fit$cvsd[maxind]))
+  }, error=function(e) {return(c(NA, NA))})
+}
+
+# find auc for each unit, filter for those with good enough zscore
+thresh <- qnorm(0.95)
+auc <- countdata %>%
+  mutate(y=outcome %in% c("other", "both")) %>%
+  select_("unit", countvar, "y", "reward") %>%
+  group_by(unit) %>%
+  do(res = train_glm(., 5)) %>%
+  filter(!is.na(res[1])) %>%
+  mutate(mean=res[1], sd=res[2]) %>%
+  filter(mean - thresh * sd > 0.5)
+
+ngood <- nrow(auc)
+ntot <- length(unique(countdata$unit))
